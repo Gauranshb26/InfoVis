e801f9315225b52915dde9ed71f2131065b08ae4
Gauranshb26 <125152227+Gauranshb26@users.noreply.github.com>
2025-03-14 09:54:25 +0000

    project_infovis.py

--- project_infovis.py
+++ project_infovis.py
@@ -15,9 +15,9 @@ state_names = [col.split("!!")[0] for col in df.columns[1::4]]  # Extract unique
 median_house_values = df.iloc[value_row_index, 1::4].astype(str).str.replace(",", "", regex=True).apply(pd.to_numeric, errors='coerce')
 median_household_incomes = df.iloc[income_row_index, 1::4].astype(str).str.replace(",", "", regex=True).apply(pd.to_numeric, errors='coerce')
 
-# Compute the average ratio of VALUE to Household Income while handling divisions by zero
+# Compute the average ratio of VALUE to Household Income 
 value_to_income_ratio = median_house_values / median_household_incomes
-value_to_income_ratio.replace([float('inf'), -float('inf')], None, inplace=True)  # Replace infinite values
+value_to_income_ratio.replace([float('inf'), -float('inf')], None, inplace=True)  
 
 # Create a cleaned DataFrame
 cleaned_df = pd.DataFrame({
